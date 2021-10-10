e820a453b0f6050f80a00b3501b15000e2c0f202
G10 <G10@DESKTOP-1OAV57S>
2021-10-10 20:21:12 +0100

    Kata2 v3

--- src/main/java/prueba/kata2/Kata2.java
+++ src/main/java/prueba/kata2/Kata2.java
@@ -16,14 +16,11 @@ public class Kata2 {
     
     public static void main(String[] args) {
         int[] data = {0, 1, 2, 0, 2, 3, 1, 4, 5, 6, 7, 5, 7, 2};
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        Histogram hist = new Histogram(data);
+        Map<Integer, Integer> histogr = hist.getHistogram();
         
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-        }
-        
-        for (int key : histogram.keySet()) {
-            System.out.println(key + "\t==>\t" + histogram.get(key));
+        for (int key : histogr.keySet()) {
+            System.out.println(key + "\t==>\t" + histogr.get(key));
         }
         
     }
